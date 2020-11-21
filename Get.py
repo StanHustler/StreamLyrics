770c03cbf0a5ec5d56fe42219d7d9acb5011a9c5
StanHustler <cf260469080@sina.com>
2020-11-21 22:52:51 +0800

    update get.py

--- Get.py
+++ Get.py
@@ -8,23 +8,38 @@ url = "http://musicapi.leanapp.cn/search?keywords=" + sname
 
 x = PrettyTable(field_names=["id", "歌名", "歌手", "专辑"])
 x.padding_width = 1
-for i in range(0, 10):
-    x.add_row([i+1,
-               json.loads(requests.get(url).text)["result"]["songs"][i]["name"],
-               json.loads(requests.get(url).text)["result"]["songs"][i]["artists"][0]["name"],
-               json.loads(requests.get(url).text)["result"]["songs"][i]["album"]["name"]])
-x.align["id"] = "l"
 
+try:
+    for i in range(0, 10):
+        x.add_row([i+1,
+                   json.loads(requests.get(url).text)["result"]["songs"][i]["name"],
+                   json.loads(requests.get(url).text)["result"]["songs"][i]["artists"][0]["name"],
+                   json.loads(requests.get(url).text)["result"]["songs"][i]["album"]["name"]])
 
-print(x.get_string())
-index = input("请输入序号（默认为1）:")
+except IndexError:
+    print("唯一匹配，自动选择")
+    sid = str(json.loads(requests.get(url).text)["result"]["songs"][0]["id"])
+    print("已定位歌曲，id为" + sid)
 
-sid = str(json.loads(requests.get(url).text)["result"]["songs"][int(index)-1]["id"])
-print("已定位歌曲，id为"+sid)
+except KeyError:
+    print("没有数据，任意键退出")
+    input()
+    exit()
+
+else:
+    x.align["id"] = "l"
+    print(x.get_string())
+    index = input("请输入序号（默认为1）:")
+    sid = str(json.loads(requests.get(url).text)["result"]["songs"][int(index)-1]["id"])
+    print("已定位歌曲，id为"+sid)
+
 status = input("①CN ②原文:")
 url = "http://music.163.com/api/song/lyric?os=pc&id="
 
-url += sid
+if sid == "":
+    url += "0"
+else:
+    url += sid
 
 if status == "1":  # cn
     url += "&tv=-1"
@@ -36,9 +51,16 @@ elif status == "2":  # orgin
 
 else:
     url += "&tv=-1&lv=-1"
+    # Waiting for commit
 
-file_handle = open(sname + '-' + sid + '.txt', mode='w+')
-print("正在写入")
-file_handle.write(lyric)
-file_handle.close()
-print("写入完成")
+_dyin_ = input("是否打印1/0")
+if _dyin_ == "1":
+    print(lyric)
+
+_bcun_ = input("是否写入文件1/0")
+if _bcun_ == "1":
+    file_handle = open(sname + '-' + sid + '.txt', mode='w+')
+    print("正在写入")
+    file_handle.write(lyric)
+    file_handle.close()
+    print("写入完成")
